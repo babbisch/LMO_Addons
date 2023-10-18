d7cec1409586f70d319843958b30fb02bbfb1ee9
dwblmo <dummwiebrot@gmail.com>
2023-10-18 17:59:27 +0200

    remove already updated files

--- stats/addon/stats/lmo-adminbasic1.inc.php
+++
@@ -1,4 +0,0 @@
-          <div class="row">
-            <div class="col-5 text-end align-self-center"><?php echo $text['stats'][0];?></div>
-            <div class="col-5 text-start"><input type="checkbox" class="form-check-input" name="xstats" onChange="dolmoedit()"<?php if($stats==1){echo " checked";}?>></div>
-          </div>
--- stats/addon/stats/lmo-adminbasic2.inc.php
+++
@@ -1,1 +0,0 @@
-<td class="nobr" align="left">&nbsp;<input type="checkbox" class="check" name="xstats" onChange="dolmoedit()"<?phpif($stats==1){echo " checked";}?>>&nbsp;<?php=$text['stats'][0];?></td>
