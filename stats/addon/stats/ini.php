ad72b122f2b799186f2cf4685d87ca7956eecd93
DwB <dummwiebrot@gmail.com>
2025-01-15 12:46:06 +0100

    Update ini.php
    constant URL removed

--- stats/addon/stats/ini.php
+++ stats/addon/stats/ini.php
@@ -25,11 +25,8 @@ if (!defined('ADDON_NAME'))
     define('ADDON_NAME', 'TEAM-Vergleich');
 if (!defined('VERSIONA'))
     define('VERSIONA', 'Copyright (C) 2004 by <a href=http://www.httipp.de.vu target=_blank>HT</a> & <a href=http://www.bwlangenberg.de target=_blank>Horten</a>');
-if (!defined('URL'))
-    define('URL', '<c:alink:https://babig.de/>https://babig.de</c:alink>');
 require_once (PATH_TO_ADDONDIR . '/classlib/classes.php');
 require_once (PATH_TO_ADDONDIR . '/classlib/functions.php');
-
 if (!defined('TEAM_VERGLEICH'))
     define('TEAM_VERGLEICH', ADDON_NAME . ' ' . VERSION . ' Addon für LMO 4');
 if (!defined('VERSlON'))
