9c8ad618b4bd68a85b49943eb6a14f04c5feae84
babbisch <dummwiebrot@gmail.com>
2025-11-04 09:58:04 +0100

    Update standard.tpl.php
    always use latest bootstrap version

--- stats/template/stats/standard.tpl.php
+++ stats/template/stats/standard.tpl.php
@@ -3,8 +3,7 @@
 <title><!--TeamA--> vs. <!--TeamB--></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=0.5,user-scalable=yes" />
-<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"	type="text/css" media="screen" />
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.min.css"	type="text/css" media="screen" />
 <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
 </head>
 <body>
@@ -46,7 +45,7 @@
   </div>
 </div>
 <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script type='text/javascript'>
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
